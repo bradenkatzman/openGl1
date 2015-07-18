bf2e17c294b7ee43ab0d0411e3d7848dd2685381
bradenkatzman <bradenkatzman@gmail.com>
2015-07-18 15:42:19 -0700

    initialized project

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  a4
+//
+//  Created by Braden Katzman on 7/18/15.
+//  Copyright (c) 2015 Braden Katzman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Wassup!\n";
+    return 0;
+}
